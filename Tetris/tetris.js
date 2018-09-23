bae2bbf3ff7831c5e66f93ef399d3f6be07d8eb6
akuma <you@example.com>
2018-09-24 01:03:34 +0300

    rotate done

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -6,6 +6,7 @@ var blackRect = "#102d0e";
 var xRange = 500;
 var yRange = 800;
 var sizeSide = 50;
+var clearing_line_is;
 
 let interval;
 var matrixFigure
@@ -32,13 +33,13 @@ class Square {
         this.y = center.y - sizeSide/2 + tmp_x;
 	}
 
-	possibleRotate(){
+	possibleRotate(center){
 		let tmp_x = this.x + sizeSide/2 - center.x;
 		let tmp_y = this.y + sizeSide/2 - center.y;
         let new_x = center.x - sizeSide/2 - tmp_y;
         let new_y = center.y - sizeSide/2 + tmp_x;
-
-		return {x: new_x, y: new_y};
+		// console.log("new_x= ", new_x, "new_y", new_y);
+		return {new_x: new_x, new_y: new_y};
 	}
 
 }
@@ -49,15 +50,15 @@ class Figure {
 		switch (figure) {
 			case "Figure_I":
 				this.figure = [ new Square(x,y), new Square(x+sizeSide,y), new Square(x+2*sizeSide,y), new Square(x+3*sizeSide,y)];
-				this.center = {x: sizeSide*5, y: sizeSide};
+				this.center = {x: sizeSide*6, y: sizeSide};
 				break; //turquoise
 			case "Figure_J":
 				this.figure = [ new Square(x,y), new Square(x,y+sizeSide), new Square(x+sizeSide, y+sizeSide), new Square(x+2*sizeSide,y+sizeSide)]; //blue
-				this.center = {x: sizeSide*5.5, y: sizeSide*0.5};;
+				this.center = {x: sizeSide*5.5, y: sizeSide*1.5};;
 				break;
 			case "Figure_L":
 				this.figure = [ new Square(x+2*sizeSide,y), new Square(x,y+sizeSide), new Square(x+sizeSide, y+sizeSide), new Square(x+2*sizeSide,y+sizeSide)]; //orange
-				this.center = {x: sizeSide*5.5, y: sizeSide*0.5};
+				this.center = {x: sizeSide*5.5, y: sizeSide*1.5};
 				break;
 			case "Figure_O":
 				this.figure = [ new Square(x,y), new Square(x+sizeSide,y), new Square(x,y+sizeSide), new Square(x+sizeSide,y+sizeSide)]; //yelow
@@ -81,17 +82,51 @@ class Figure {
 	}
 
 	rotate(){
-		this.clear();
+
 		if(this.flag_move){
 			if(this.canRotate()){
+				this.clear();
 				for (let sq of this.figure) {
 					// console.log("x = ", sq.x, "y = ", sq.y);
-					console.log("center x = ", this.center.x, "center y = ", this.center.y);
+					// console.log("fcenter x = ", this.center.x, "center y = ", this.center.y);
 					sq.rotate(this.center);
 				}
 			}
 		}
         this.draw();
+	}
+	canRotate(){
+		let cnt = 0;
+		let x_min = Math.min.apply(null, this.figure.map(item => item.x));
+		let x_max = Math.max.apply(null, this.figure.map(item => item.x));
+		var y_max = 0;
+
+		for(let i of this.figure){
+			let new_coords = i.possibleRotate(this.center);
+			if(y_max < new_coords.new_y)
+				y_max = new_coords.new_y;
+
+			if( matrixFigure[new_coords.new_y/sizeSide][new_coords.new_x/sizeSide] != undefined && !matrixFigure[new_coords.new_y/sizeSide][new_coords.new_x/sizeSide] /*&& new_coords.new_y < yRange - sizeSide */){
+				cnt++;
+			}
+			console.log("new coordinates x = ", new_coords.new_x, "y = ", new_coords.new_y);
+			console.log("x = ", i.x, "y = ", i.y);
+			console.log(matrixFigure[new_coords.new_y/sizeSide][new_coords.new_x/sizeSide]);
+		}
+
+		if(y_max > yRange){
+			console.log("RANNGEE");
+			return false;
+		}
+
+		if(cnt === this.figure.length){
+			console.log("TRUEEEEEEEEE");
+			console.log(matrixFigure);
+
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 	clear(){
@@ -99,7 +134,6 @@ class Figure {
 			ctx.clearRect(i.x, i.y, sizeSide+1, sizeSide+1);
 		}
 	}
-
 	draw() {
 		for (let i of this.figure) {
 				draw(i.x, i.y);
@@ -122,7 +156,6 @@ class Figure {
 		}
 		this.draw();
 	}
-
 	left(){
 		this.clear();
 		let x_min = Math.min.apply(null, this.figure.map(item => item.x));
@@ -136,7 +169,6 @@ class Figure {
 		}
 		this.draw();
 	}
-
 	right(){
 		this.clear();
 		let x_max = Math.max.apply(null, this.figure.map(item => item.x));
@@ -164,7 +196,6 @@ class Figure {
 			return false;
 		}
 	}
-
 	canMoveLeft(){
 		let cnt = 0; //let x_min = Math.min.apply(null, this.figure.map(item => item.x));
 		for(let i of this.figure){
@@ -178,7 +209,6 @@ class Figure {
 			return false;
 		}
 	}
-
 	canMoveRight(){
 		let cnt = 0; //let x_min = Math.min.apply(null, this.figure.map(item => item.x));
 		for(let i of this.figure){
@@ -192,21 +222,6 @@ class Figure {
 			return false;
 		}
 	}
-
-	canRotate(){
-		let cnt = 0; //let x_min = Math.min.apply(null, this.figure.map(item => item.x));
-		for(let i of this.figure){
-			if( !matrixFigure[i.y/sizeSide][i.x/sizeSide+1] ){
-				cnt++;
-			}
-		}
-		if(cnt === this.figure.length){
-			return true;
-		}else {
-			return false;
-		}
-	}
-
 }
 
 document.addEventListener('keydown', (event) => {
@@ -258,12 +273,10 @@ function checkOnLose() {
 }
 
 function checkFullLine() {
-	var clearing_line_is;
-	var count = 0;
-	for (var i = 1; i < 17; i++) {
-		count = 0;
-		for (var j = 0; j < 10; j++) {
-			if(matrixFigure[i][j] && i != 0){
+	for (var i = yRange/sizeSide; i < 1; i++) {
+		 let count = 0;
+		for (var j = 0; j < xRange/sizeSide; j++) {
+			if(matrixFigure[i][j]){
 				count++;
 			}
 			console.log("count=", count);
@@ -288,7 +301,7 @@ function clearLine() {
 
 function recountMatrix() {
 	for (let i = clearing_line_is; i > 1; i--){
-        matrix[i] = matrix[i-1];
+        matrixFigure[i] = matrixFigure[i-1];
     }
 }
 
@@ -301,22 +314,25 @@ function startGame() {
 		f.draw();
 		f.down();
 	}else {
-		// console.log(matrixFigure);
-
-		// if(checkFullLine())
-		// 	clearLine();
+		console.log("FLAG MOVE IS FALSE");
+		if(checkFullLine()){
+			clearLine();
+			console.log("CHHHHHHEEEK");
+		}
 		// if(checkOnLose()){
 		// 	endGame();
 		// }
 
-		f = new Figure(200, 0, figureArr[Math.floor(Math.random()*7)]); // new Figure(200, 0, "Figure_O");// */new Figure(200, 0, "Figure_I");
+		// f = new Figure(200, 0, figureArr[Math.floor(Math.random()*7)]); // new Figure(200, 0, "Figure_O");// */new f
+		f = new Figure(200, 0, "Figure_I");
 	}
 }
 
 function newGame() {
 	clearInterval(interval);
 	console.log("newGame");
-	f = new Figure(200, 0, figureArr[Math.floor(Math.random()*7)]); //	new Figure(200, 0, "Figure_O"); //new Figure(200, 0, figureArr[Math.floor(Math.random()*7)]);
+	// f = new Figure(200, 0, figureArr[Math.floor(Math.random()*7)]);
+	f = new Figure(200, 0, "Figure_I");
 	score = 0;
 	interval = setInterval( startGame, 500 );
 
